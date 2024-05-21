93cbdba61c1b4fdb15f17fa2bcd284b1279ad74e
ender7396 <lss933673961234@gmail.com>
2024-05-21 10:19:37 +0900

    플레이어 이동 그리고 주변 빛

---
+++ Assets/JungBIn/Scripts/PlayerMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * moveSpeed;
+
+        rb.velocity = moveVelocity;
+    }
+
+}
