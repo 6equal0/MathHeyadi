a4d227801606a48c03b0fe0877a9fd8151b1035e
6equal0 <6equal0@gmail.com>
2024-05-22 20:29:01 +0900

    경직 만들기~

--- Assets/Geun0/Scripts/EnemyAI.cs
+++ Assets/Geun0/Scripts/EnemyAI.cs
@@ -9,13 +9,18 @@ public class EnemyAI : MonoBehaviour
     private NavMeshAgent agent;
     private PlayerTest player;
     private Vector3 target;
+    private bool isParalized = false;
+    private float tempParalizedTime;
+    private float standardParalizedTime = 10f;
 
     private enum State
     {
         IDLE,
         CHASE,
         PATROL,
-        PARALIZED,
+        PARALIZED1,
+        PARALIZED2,
+        PARALIZED3,
     }
 
     [SerializeField] private State states;
@@ -30,6 +35,10 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] private Color patrolColor;
     [SerializeField] private Color paralizedColor;
 
+    [SerializeField] private float paralizedTime1 = 0.5f;
+    [SerializeField] private float paralizedTime2 = 1.5f;
+    [SerializeField] private float paralizedTime3 = 3f;
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -41,8 +50,8 @@ public class EnemyAI : MonoBehaviour
     private void Update()
     {
         agent.destination = target;
-        Debug.Log(agent.destination);
-        Debug.Log(target);
+
+        InputTest();
 
         switch (states)
         {
@@ -55,10 +64,36 @@ public class EnemyAI : MonoBehaviour
             case State.PATROL:
                 PatrolUpdate();
                 break;
-            case State.PARALIZED:
-                ParalizedUpdate();
+            case State.PARALIZED1:
+                Paralized1Update();
+                break;
+            case State.PARALIZED2:
+                Paralized2Update();
+                break;
+            case State.PARALIZED3:
+                Paralized3Update();
                 break;
         }
+    }
+
+    private void InputTest()
+    {
+        if (!isParalized)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                states = State.PARALIZED1;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                states = State.PARALIZED2   ;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                states = State.PARALIZED3;
+            }
+        }
+
     }
 
     private void IdleUpdate()
@@ -69,7 +104,7 @@ public class EnemyAI : MonoBehaviour
         if(Vector2.Distance(transform.position, player.transform.position) <= chaseRange)
         {
             states = State.CHASE;
-            transform.GetComponent<SpriteRenderer>().DOColor(Color.red, 1f).SetEase(Ease.OutQuint);
+            transform.GetComponent<SpriteRenderer>().DOColor(chaseColor, 1f).SetEase(Ease.OutQuint);
         }
     }
 
@@ -80,7 +115,7 @@ public class EnemyAI : MonoBehaviour
         if(Vector2.Distance(transform.position, player.transform.position) > patrolRange)
         {
             states = State.PATROL;
-            transform.GetComponent<SpriteRenderer>().DOColor(Color.yellow, 2f).SetEase(Ease.OutSine);
+            transform.GetComponent<SpriteRenderer>().DOColor(patrolColor, 1f).SetEase(Ease.OutSine);
         }
     }
 
@@ -93,14 +128,78 @@ public class EnemyAI : MonoBehaviour
 
         if (Vector2.Distance(transform.position, player.transform.position) <= patrolRange)
         {
-            transform.GetComponent<SpriteRenderer>().DOColor(Color.red, 1f).SetEase(Ease.OutQuint);
+            transform.GetComponent<SpriteRenderer>().DOColor(chaseColor, 1f).SetEase(Ease.OutQuint);
             states = State.CHASE;
         }
     }
 
-    private void ParalizedUpdate()
+    private void Paralized1Update()
     {
+        if (isParalized)
+        {
+            tempParalizedTime -= (standardParalizedTime / paralizedTime1);
 
+            if(tempParalizedTime < 0)
+            {
+                isParalized = false;
+                tempParalizedTime = standardParalizedTime;
+
+                states = State.PATROL;
+                transform.GetComponent<SpriteRenderer>().DOColor(patrolColor, 1f).SetEase(Ease.OutSine);
+            }
+        }
+        else
+        {
+            isParalized = true;
+            agent.destination = transform.position;
+            transform.GetComponent<SpriteRenderer>().DOColor(paralizedColor, 0.5f).SetEase(Ease.OutQuint);
+        }
+    }
+
+    private void Paralized2Update()
+    {
+        if (isParalized)
+        {
+            tempParalizedTime -= (standardParalizedTime / paralizedTime2);
+
+            if (tempParalizedTime < 0)
+            {
+                isParalized = false;
+                tempParalizedTime = standardParalizedTime;
+
+                states = State.PATROL;
+                transform.GetComponent<SpriteRenderer>().DOColor(patrolColor, 1f).SetEase(Ease.OutSine);
+            }
+        }
+        else
+        {
+            isParalized = true;
+            agent.destination = transform.position;
+            transform.GetComponent<SpriteRenderer>().DOColor(paralizedColor, 0.5f).SetEase(Ease.OutSine);
+        }
+    }
+
+    private void Paralized3Update()
+    {
+        if (isParalized)
+        {
+            tempParalizedTime -= (standardParalizedTime / paralizedTime3);
+
+            if (tempParalizedTime < 0)
+            {
+                isParalized = false;
+                tempParalizedTime = standardParalizedTime;
+
+                states = State.PATROL;
+                transform.GetComponent<SpriteRenderer>().DOColor(patrolColor, 1f).SetEase(Ease.OutSine);
+            }
+        }
+        else
+        {
+            isParalized = true;
+            agent.destination = transform.position;
+            transform.GetComponent<SpriteRenderer>().DOColor(paralizedColor, 0.5f).SetEase(Ease.OutSine);
+        }
     }
 
     private void OnDrawGizmos()
