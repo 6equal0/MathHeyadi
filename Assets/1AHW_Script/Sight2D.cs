a5c8fc7cb6254076837ae22ed48695d2b05c2555
antking1234 <123617733+antking1234@users.noreply.github.com>
2024-05-23 16:27:04 +0900

    findTarget public으로 수정

--- Assets/1AHW_Script/Sight2D.cs
+++ Assets/1AHW_Script/Sight2D.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Sight2D : MonoBehaviour
 {
     Vector2 playerTarget;
-    [SerializeField] private bool findTarget= false;
+    [SerializeField] public bool findTarget= false;   //플레이어 오브젝트 발견 여부
     [SerializeField] private bool m_bDebugMode = false;
 
     [Header("View Config")]
@@ -54,11 +54,11 @@ public class Sight2D : MonoBehaviour
             float angle = Mathf.Acos(dot) * Mathf.Rad2Deg;
 
 
-            if (angle>=m_horizontalViewAngle)
+            if (angle>=m_horizontalViewAngle) // 범위 안에 아무것드 없을 때
             {
                 findTarget = false; 
             }
-            else if (angle <= m_horizontalViewHalfAngle && hitedTarget.CompareTag("Player"))
+            else if (angle <= m_horizontalViewHalfAngle && hitedTarget.CompareTag("Player")) //범위 안에 Player 태그를 가진 오브젝트가 있을 때
             {
                 RaycastHit2D rayHitedTarget = Physics2D.Raycast(originPos, dir, m_viewRadius, m_viewObstacleMask);
                 if (rayHitedTarget)
